f63107c763a0b99d0ae2d16f4400ac17927a4781
TasnimJumara <tasnimjumara@gmail.com>
2026-05-08 15:22:03 +0600

    Who will pay the bill?

--- Pizza Order Practice (Day 3).py
+++ Pizza Order Practice (Day 3).py
@@ -1,9 +1,12 @@
+import random
 print("Welcome to Pizza Order Deliveries!")
 
 size = input("What size of pizza do you want? S, M, or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
+colleagues = ["Rahnuma", "Eva", "Mithyla", "Raka", "Tasnim", "Effat", "Rokeya", "Ripa"]
+bill_pay = random.choice(colleagues)
 price = 0
 
 if size == "S":
@@ -24,5 +27,6 @@ else:
 if extra_cheese == "Y":
     price += 1
 
-print(f"Your final bill is ${price}.")
+print(f"Your final bill is ${price} and the bill will be paid by {bill_pay}.")
+
 
